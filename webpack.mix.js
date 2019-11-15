0a0e9f977fc5377d324b8bf27a865810d28b3916
Roberto <ritorito141516@gmail.com>
2019-11-15 18:17:09 -0500

    Configurando la instalación de Tailwind CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,14 @@
 const mix = require('laravel-mix')
+const tailwindcss = require('tailwindcss')
+
 const path = require('path')
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
+   .options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind.config.js') ],
+  })
    .webpackConfig({
      output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
      resolve: {
